453bb779ca81ee3c6d4f9d711197ef624ab60509
donquxiote <donquixote42@malteseplatypus.com>
2019-08-16 22:45:47 -0500

    brings encrypt.js up to par with Standard JS linting

--- gulp_scripts/encrypt.js
+++ gulp_scripts/encrypt.js
@@ -1,20 +1,19 @@
-'use strict';
+'use strict'
 
-const PluginError = require('plugin-error');
-const cryptojs = require('crypto-js');
-const marked = require('marked');
-const through = require('through2');
+const PluginError = require('plugin-error')
+const cryptojs = require('crypto-js')
+const marked = require('marked')
+const through = require('through2')
 
-
-function exportedBody() { };
+function exportedBody () { };
 
 module.exports = {
   exportedBody: exportedBody,
   encrypt: function (password) {
     return through.obj(function (file, encoding, callback) {
       if (file.isNull() || file.isDirectory()) {
-        this.push(file);
-        return callback();
+        this.push(file)
+        return callback()
       }
 
       // No support for streams
@@ -22,56 +21,56 @@ module.exports = {
         this.emit('error', new PluginError({
           plugin: 'Encrypt',
           message: 'Streams are not supported.'
-        }));
-        return callback();
+        }))
+        return callback()
       }
 
       if (file.isBuffer()) {
-        var delimiter = '---',
-          chunks = String(file.contents).split(delimiter),
-          originalBody = chunks[0],
-          frontMatter = '';
+        var delimiter = '---'
+        var chunks = String(file.contents).split(delimiter)
+        var originalBody = chunks[0]
+        var frontMatter = ''
 
         if (chunks.length === 3) {
-          checkEncryptedLayout(chunks[1], file.path);
-          frontMatter = chunks[1];
-          originalBody = chunks[2];
+          checkEncryptedLayout(chunks[1], file.path)
+          frontMatter = chunks[1]
+          originalBody = chunks[2]
         } else if (chunks.length > 1) {
           this.emit('error', new PluginError({
             plugin: 'Encrypt',
             message: file.path + ': protected file has invalid front matter.'
-          }));
-          return callback();
+          }))
+          return callback()
         }
 
-        var encryptedBody = cryptojs.AES.encrypt(marked(originalBody), password),
-          hmac = cryptojs.HmacSHA256(encryptedBody.toString(), cryptojs.SHA256(password).toString()).toString();
+        var encryptedBody = cryptojs.AES.encrypt(marked(originalBody), password)
+        var hmac = cryptojs.HmacSHA256(encryptedBody.toString(), cryptojs.SHA256(password).toString()).toString()
 
-        module.exports.exportedBody.body = hmac + encryptedBody;
-        return callback();
+        module.exports.exportedBody.body = hmac + encryptedBody
+        return callback()
       }
-    });
+    })
   }
-};
+}
 
-function checkEncryptedLayout(frontMatter, filepath) {
-  var lines = frontMatter.split('\n'),
-    linesWithoutLayout = [],
-    hasEncryptedLayout = false;
+function checkEncryptedLayout (frontMatter, filepath) {
+  var lines = frontMatter.split('\n')
+  // var linesWithoutLayout = []
+  var hasEncryptedLayout = false
 
   lines.forEach(function (line) {
-    var layoutTag = 'layout:',
-      isLayoutIndex = line.indexOf(layoutTag),
-      isLayout = isLayoutIndex >= 0,
-      isEncryptedLayout = line.indexOf('encrypted') >= (isLayoutIndex + layoutTag.length);
+    var layoutTag = 'layout:'
+    var isLayoutIndex = line.indexOf(layoutTag)
+    var isLayout = isLayoutIndex >= 0
+    var isEncryptedLayout = line.indexOf('encrypted') >= (isLayoutIndex + layoutTag.length)
 
     if (isLayout) {
       // in case of multiple instances of layout
-      hasEncryptedLayout = isEncryptedLayout ? true : false;
+      hasEncryptedLayout = isEncryptedLayout === true
     }
-  });
+  })
 
   if (!hasEncryptedLayout) {
-    console.log('[WARNING] ' + filepath + ': protected file not using encrypted layout.');
+    console.log('[WARNING] ' + filepath + ': protected file not using encrypted layout.')
   }
 };
