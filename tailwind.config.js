5a678d977fd699ef105f58100f2ba8b288b1a575
donquxiote <github@q42.email>
2023-02-23 20:55:17 -0600

    more tailwind fixes and getting it up to snuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,14 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
+    colors: {
+      'background': '#1E1E3F',
+      'text': '#A599E9',
+      'link': '#FAD000',
+      'link_visited': '#6943FF',
+    },
     extend: {
-      maxWidth: {'1/2': '50%'},
+      maxWidth: { '1/2': '50%' },
     },
     fontFamily: {
       'sans': ['Fira Sans', 'sans-serif']
